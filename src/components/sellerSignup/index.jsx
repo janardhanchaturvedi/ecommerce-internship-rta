062757788a345644a1fc5ddc20f06e3f21e106c1
Your Name <jan.cj84@gmail.com>
2026-02-19 16:34:14 +0530

    fixed some issues

--- src/components/sellerSignup/index.jsx
+++ src/components/sellerSignup/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Header } from "@/components/layout/Header";
-import { Footer } from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
